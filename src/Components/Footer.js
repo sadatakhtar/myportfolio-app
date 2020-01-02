0868617c41f7c93bf6ce1f6e645382394884e62b
sadat akhtar <sadat0akhtar@gmail.com>
2020-01-02 01:37:30 +0000

    changed footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ import React from 'react';
 function Footer(){
     return (
         <footer>
-            <h5>Copyright &copy; 2019 Sadat Akhtar.</h5>
+            <h5>Copyright &copy; 2019 Portfolio by Sadat Akhtar.</h5>
         </footer>
         
     )
