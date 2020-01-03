3b2ac2efa5f2475722779dabccebbadeab1a28df
sadat akhtar <sadat0akhtar@gmail.com>
2020-01-03 14:58:26 +0000

    added main-div element to div tag in  About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import Icons from './Icons';
 
 function About(){
     return (
-        <div>
+        <div id="main-div">
         <h1>About page construction in progress!</h1>
         <p id="skills">HTML | CSS | JavaScript | React | NodeJS | Python</p>
         <Icons />
