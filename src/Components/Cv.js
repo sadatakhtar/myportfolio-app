e97498b42887611122434ebc1c0494fc1d7e1a09
sadat akhtar <sadat0akhtar@gmail.com>
2020-01-03 15:00:57 +0000

    added main-div element to div tag in  Cv.js

--- src/Components/Cv.js
+++ src/Components/Cv.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Icons from './Icons';
 
 function Cv(){
     return (
+        <div id="main-div">
         <h1>CV Page coming soon!</h1>
+        <p id="skills">HTML | CSS | JavaScript | React | NodeJS | Python</p>
+        <Icons />
+        </div>
     )
 }
 export default Cv;
