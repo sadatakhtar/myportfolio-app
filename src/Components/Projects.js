9fd8c115914788d5add4cbd50051037c7d05a828
Sadat <sadat0akhtar@gmail.com>
2020-04-27 14:36:08 +0100

    added github page to tribute page project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,7 +10,7 @@ function Projects(){
         <h1>Projects</h1>
         <p id="project-img"><img src={data} alt="web logo" height="50px" width="50px" /></p>
         <ol>
-            <li><a id="pro-links" href="https://codepen.io/shadowak47/pen/QxpgXL" target="_blank">Tribute Page</a></li>
+            <li><a id="pro-links" href=" https://sadatakhtar.github.io/CodePen_Export_QxpgXL/" target="_blank">Tribute Page</a></li>
             <li><a id="pro-links" href="https://www.khanacademy.org/computing/computer-programming/programming/animation-basics/pp/project-shooting-star" target="_blank"> Khan academy-Shooting star</a></li>
             <li><a id="pro-links" href="https://www.khanacademy.org/computer-programming/spin-off-of-project-build-a-house/6301463741612032" target="_blank">Khan academy-Build a house</a></li>
             <li><a id="pro-links" href="https://www.khanacademy.org/computer-programming/spin-off-of-project-fish-tank/4799744209731584" target="_blank">Khan academy-Fish tank</a></li>
