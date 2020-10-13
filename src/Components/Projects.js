05e25ffc2c1f42b03ef0a1ff36d2401457b4c015
sadatakhtar <sadat0akhtar@gmail.com>
2020-10-13 21:25:50 +0100

    cleared out old projects and added netflix clone

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,12 +10,8 @@ function Projects(){
         <h1>Projects</h1>
         <p id="project-img"><img src={data} alt="web logo" height="50px" width="50px" /></p>
         <ol>
-            <li><a id="pro-links" href=" https://sadatakhtar.github.io/CodePen_Export_QxpgXL/" target="_blank">Tribute Page</a></li>
-            <li><a id="pro-links" href="https://www.khanacademy.org/computing/computer-programming/programming/animation-basics/pp/project-shooting-star" target="_blank"> Khan academy-Shooting star</a></li>
-            <li><a id="pro-links" href="https://www.khanacademy.org/computer-programming/spin-off-of-project-build-a-house/6301463741612032" target="_blank">Khan academy-Build a house</a></li>
-            <li><a id="pro-links" href="https://www.khanacademy.org/computer-programming/spin-off-of-project-fish-tank/4799744209731584" target="_blank">Khan academy-Fish tank</a></li>
-            <li><a id="pro-links" href="https://sadatakhtar.github.io/fcc-landing/" target="_blank">Landing Page</a></li>
-            <li><a id="pro-links" href="https://sadatakhtar.github.io/fcc-survey/." target="_blank">Survey Form</a></li>
+           
+            <li><a id="pro-links" href="https://netflix-clone-16e6a.web.app/" target="_blank">Netflix Clone</a></li>
             <li><a id="pro-links" href="https://github.com/sadatakhtar/py-newsFeeder/blob/master/newsFeeder.py"
              target="_blank">Python NewsFeeder</a></li>
             <li><a id="pro-links" href="https://github.com/sadatakhtar/net-H-toolz/blob/master/netH.py" 
