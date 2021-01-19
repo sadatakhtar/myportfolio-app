455d6b5486c2b4895c633311143549a2824796b9
sadatakhtar <sadat0akhtar@gmail.com>
2021-01-19 12:15:40 +0000

    updated some project url's

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,13 +10,12 @@ function Projects(){
         <h1>Projects</h1>
         <p id="project-img"><img src={data} alt="web logo" height="50px" width="50px" /></p>
         <ol>
-           
+            <li><a id="pro-links" href="https://question-mark.netlify.app/">Q&A platform</a></li>
             <li><a id="pro-links" href="https://netflix-clone-16e6a.web.app/" >Netflix Clone</a></li>
-            <li><a id="pro-links" href="https://myspotify-clone.netlify.app/" >Spotify Clone</a></li>
+            <li><a id="pro-links" href="https://cyf-student-tracker-1.herokuapp.com" >CYF-student tracker</a></li>
             <li><a id="pro-links" href="https://cyf-sadatakhtar-countries.netlify.app/" >Countries App</a></li>
             <li><a id="pro-links" href="https://cyf-sadatakhtar-tv.netlify.app/">Tv shows</a></li>
-            <li><a id="pro-links" href="https://random-quotes-appserver.netlify.app/" 
-            >Random quotes</a></li>
+            <li><a id="pro-links" href="https://random-quotes-appserver.netlify.app/">Random quotes</a></li>
             
         </ol>
         <p id="skills">HTML | CSS | JavaScript | React | NodeJS | Python</p>
