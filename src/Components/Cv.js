acd02bf3f262d6b796c4b4472e9b8ab04aab0caf
sadatakhtar <sadat0akhtar@gmail.com>
2021-07-30 22:49:25 +0100

    updated latest CV

--- src/Components/Cv.js
+++ src/Components/Cv.js
@@ -8,7 +8,7 @@ function Cv(){
 
         <h1>Curriculum Vitae</h1>
         <p><img src={curriculam} alt="cv-logo" width="60px" height="70px" /></p>
-        <h2 id="cv-h2"><a id="cv-link" href="https://docs.google.com/document/d/1G4FfqLL1H-fgd2u5E1euYwbFfDHMviE_bkei4VnWyhE/edit?usp=sharing" 
+        <h2 id="cv-h2"><a id="cv-link" href="https://docs.google.com/document/d/1p9-4aXI951dotOAKszdt2rsy6_8OU8vAVxph7cTyLuQ/edit" 
         >Click here to view CV</a></h2>
         <p id="skills">HTML | CSS | JavaScript | React | NodeJS | Python</p>
         <Icons />
